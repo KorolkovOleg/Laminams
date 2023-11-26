f2cca676c7f673a4c4923f3dc0380b6477564554
KorolkovOleg <helkorolkov@gmail.com>
2023-11-26 06:53:13 +0300

    initialising

---
+++ src/main/java/com/laminams/Laminams/LaminamsApplication.java
@@ -0,0 +1,13 @@
+package com.laminams.Laminams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaminamsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaminamsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laminams/Laminams/LaminamsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laminams.Laminams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaminamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
