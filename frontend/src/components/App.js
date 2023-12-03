e7718f1e74079719e8f1a32b54b0acb422be0cb1
KorolkovOleg <helkorolkov@gmail.com>
2023-12-03 21:30:09 +0300

    frontend init

---
+++ frontend/src/components/App.js
@@ -0,0 +1,15 @@
+import "./App.css"
+
+import Header from './Header'
+import AppContent from "./AppContent";
+
+function App() {
+    return(
+       <div>
+            <Header pageTitle = "Authentication"/>
+            <AppContent></AppContent>
+       </div>
+    );
+}
+
+export default App;
---
+++ frontend/src/components/WelcomeContent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default class WelcomeContent extends React.Component{
+    render() {
+        return (
+            <div>
+                <h1>Welcome</h1>
+            </div>
+        );
+    }
+}
